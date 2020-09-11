dbc6e88ba1f66f325089800c0400c90c464f9c84
yokemin <woonyokemin@gmail.com>
2020-09-11 17:07:12 +0800

    Deal with merge conflicts and keep both codes (Manually included)

--- src/main/java/circus/Circus.java
+++ src/main/java/circus/Circus.java
@@ -3,6 +3,8 @@ package circus;
 import circus.animal.Animal;
 import circus.animal.Duck;
 import circus.animal.Parrot;
+import circus.animal.Tiger;
+import circus.stuff.Cage;
 import circus.stuff.Cannon;
 import circus.stuff.Equipment;
 import circus.stuff.Ladder;
@@ -76,6 +78,30 @@ public class Circus {
         // remove from array list
         animalArrayList.remove(louie);
         System.out.println("Index of Louie: " + animalArrayList.indexOf(louie));
-        
+
+        makeAnimalsTalk();;
+        System.out.println("Total value of equipments " + calculateValue(equipments));
+        System.out.println("Total value of animals " + calculateValue(animals));
+
+        Cage<Duck> duckCage = new Cage<>();
+        Duck duck = new Duck("Huey");
+        duckCage.lockUp(duck);
+
+        Parrot parrot = new Parrot("Popper");
+        Cage<Parrot> parrotCage = new Cage<>();
+        parrotCage.lockUp(parrot);
+
+        Tiger tiger = new Tiger("Sherkhan");
+        Cage<Tiger> tigerCage = new Cage<>();
+        tigerCage.lockUp(tiger);
+
+        ArrayList<Cage> cages = new ArrayList<>();
+        cages.add(duckCage);
+        cages.add(parrotCage);
+        cages.add(tigerCage);
+
+        for(Cage c: cages) {
+            c.release();
+        }
     }
 }
